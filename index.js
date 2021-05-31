77364df8e0d277d90b255094dca650240921b430
Guipellegr <g173879@dac.unicamp.br>
2021-05-31 13:13:04 -0300

    Correção

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
  var cors = require('cors');
  app.use(cors());
 
- app.listen(3000);
+ app.listen(process.env.PORT || 3000);
 
  app.get('/', function (req, res){
         res.send("Hello World!");
