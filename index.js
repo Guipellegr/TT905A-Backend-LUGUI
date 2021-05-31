497e6abb807736af97cc4a1a986b7bb0e97c0aa6
Guipellegr <g173879@dac.unicamp.br>
2021-05-31 02:30:04 -0300

    Array de mensagem

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ app.listen(3000);
 app.get('/', function (req, res){
     res.send("Hello Word");
 });
+
+app.get('/hello', function (req, res){
+    res.send("Hello de novo");
+});
+
+const mensagens = [
+    "Cachorro", "Gato"
+];
+
+app.get('/mensagens', function (req, res){
+    res.send(mensagens);
+});
+
+app.get('/mensagens/ :id', function (req, res){
+    const id = req.params.id - 1;
+    const mensagem = mensagens[id];
+    if(!mensagens){
+        res.send("Mensagem não encontrada");
+    } els{
+        res.send(mensagem);
+    };
+});
